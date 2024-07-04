728835626819e7ea8d3cb3f4eaab8c6a23cf65bf
Nancy Tesfaye <nancy23tes@gmail.com>
2024-07-04 12:03:54 +0300

    header and footer added

---
+++ netflix-clone/src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Header.css";
+import netflixlogo from "../../assets/images/NetflixLogo.png";
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+const Header = () => {
+  return (
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={netflixlogo} alt="Netflix Logo" width="100" />
+            </li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- netflix-clone/src/pages/home/Home.jsx
+++ netflix-clone/src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Header />
+    <Footer />
+    </>
   )
 }
