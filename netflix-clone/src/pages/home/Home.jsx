dfd091ee4c8028c0e20cf5087dc580c029b6d3db
Nancy Tesfaye <nancy23tes@gmail.com>
2024-07-09 13:29:20 +0300

    Row component added

--- netflix-clone/src/pages/home/Home.jsx
+++ netflix-clone/src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/Banner/Banner'
+import { Rowlist } from '../../components/Rows/Rowlist/Rowlist'
 
 export const Home = () => {
   return (
     <>
     <Header />
+    <Banner />
+    <Rowlist />
     <Footer />
     </>
   )
